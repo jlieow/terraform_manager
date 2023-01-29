67346dca1e4239f12175b81b5d82a89151cb759a
jlieow <52845474+jlieow@users.noreply.github.com>
2023-01-29 21:44:40 +0800

    Created basic structure for terraformx cli

--- terraformx/destroy.py
+++ terraformx/destroy.py
@@ -1,15 +1,10 @@
-# importing required modules
-import argparse
- 
-# create a parser object
-parser = argparse.ArgumentParser(description = "An addition program")
- 
-# arguments
-parser.add_argument("-var-file", type = str, help = "Location of variable definitions file.")
-parser.add_argument("-auto-approve", action="store_true", help = "Auto approve command.")
- 
-# parse the arguments from standard input
-args = parser.parse_args()
- 
-# check if add argument has any input data.
-print("Printing all args: %s" % args)
+def destroy(args):
+    print("terraformx destroy")
+    print(args)
+    print(args.var_file)
+    print(args.auto_approve)
+    print(args.refresh_only)
+
+    var_file = args.var_file
+    auto_approve = args.auto_approve
+    refresh_only = args.refresh_only
