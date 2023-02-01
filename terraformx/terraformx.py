1f6fb6e2fb3459c67613cff6ddebd0290ab9c97b
jlieow <52845474+jlieow@users.noreply.github.com>
2023-02-01 11:38:37 +0800

    Update strings with constants

--- terraformx/terraformx.py
+++ terraformx/terraformx.py
@@ -7,6 +7,36 @@ from apply import *
 from destroy import *
 from output import *
 
+class Parser:
+    INIT = "init"
+    APPLY = "apply"
+    DESTROY = "destroy"
+    OUTPUT = "output"
+    BLUEPRINTS = "blueprints"
+    HISTORY = "history"
+class Args:
+    DIR = "-dir"
+    VAR_FILE = "-var-file"
+    AUTO_APPROVE = "-auto-approve"
+    OVERRIDE_WORKFLOW = "-override-workflow"
+    REFRESH_ONLY = "-refresh-only"
+    DESTROY_HISTORY = "-destroy-history"
+
+class Action:
+    STORE_TRUE = "store_true"
+
+class Default:
+    EMPTY_STRING = ""
+
+class Help:
+    LOCATION_OF_TERRAFORM_ROOT = "Location of terraform root."
+    LOCATION_OF_TFVARS_FILE = "Location of variable definitions file."
+    AUTO_APPROVE_COMMAND = "Auto approve command without requiring user input."
+    OVERRIDE_WORKFLOW = "Overrides workflow stages auto_approve keys and auto approves every stage."
+    TERRAOFORM_STATE_FILE_REVIEW = "Review how terraform would update your state file."
+    DESTORY_ALL_IN_HISTORY = "Destroys all in terraform_history.csv."
+
+
 top_level_help_message = "Usage: terraformx [global options] <subcommand> [args]\n\
 \n\
 The available commands for execution are listed below.\n\
@@ -29,31 +59,31 @@ def main():
 
     subparsers = top_level_parser.add_subparsers(dest="command", help=top_level_help_message)
 
-    terraformx_init = subparsers.add_parser("init")
+    terraformx_init = subparsers.add_parser(Parser.INIT)
     terraformx_init.set_defaults(function=init)
-    terraformx_init.add_argument("-dir", type = str, default="", help = "Location of terraform root.")
-    terraformx_init.add_argument("-var-file", type = str, default="", help = "Location of variable definitions file.")
+    terraformx_init.add_argument(Args.DIR, type = str, default=Default.EMPTY_STRING, help = Help.LOCATION_OF_TERRAFORM_ROOT)
+    terraformx_init.add_argument(Args.VAR_FILE, type = str, default=Default.EMPTY_STRING, help = Help.LOCATION_OF_TFVARS_FILE)
 
-    terraformx_apply = subparsers.add_parser("apply")
+    terraformx_apply = subparsers.add_parser(Parser.APPLY)
     terraformx_apply.set_defaults(function=apply)
-    terraformx_apply.add_argument("-dir", type = str, default="", help = "Location of terraform root.")
-    # terraformx_apply.add_argument("-var-file", type = str, default="", help = "Location of variable definitions file.")
-    terraformx_apply.add_argument("-auto-approve", action="store_true", help = "Auto approve command.")
-    terraformx_apply.add_argument("-override-workflow", action="store_true", help = "Overrides workflow stages auto_approve keys and auto approves every stage.")
-    terraformx_apply.add_argument("-refresh-only", action="store_true", help = "Review how terraform would update your state file.")
+    terraformx_apply.add_argument(Args.DIR, type = str, default=Default.EMPTY_STRING, help = Help.LOCATION_OF_TERRAFORM_ROOT)
+    # terraformx_apply.add_argument(Args.VAR_FILE, type = str, default=Default.EMPTY_STRING, help = Help.LOCATION_OF_TFVARS_FILE)
+    terraformx_apply.add_argument(Args.AUTO_APPROVE, action=Action.STORE_TRUE, help = Help.AUTO_APPROVE_COMMAND)
+    terraformx_apply.add_argument(Args.OVERRIDE_WORKFLOW, action=Action.STORE_TRUE, help = Help.OVERRIDE_WORKFLOW)
+    terraformx_apply.add_argument(Args.REFRESH_ONLY, action=Action.STORE_TRUE, help = Help.TERRAOFORM_STATE_FILE_REVIEW)
 
-    terraformx_destroy = subparsers.add_parser("destroy")
+    terraformx_destroy = subparsers.add_parser(Parser.DESTROY)
     terraformx_destroy.set_defaults(function=destroy)
-    terraformx_destroy.add_argument("-dir", type = str, default="", help = "Location of terraform root.")
-    # terraformx_apply.add_argument("-var-file", type = str, default="", help = "Location of variable definitions file.")
-    terraformx_destroy.add_argument("-auto-approve", action="store_true", help = "Auto approve command.")
-    terraformx_destroy.add_argument("-override-workflow", action="store_true", help = "Overrides workflow stages auto_approve keys and auto approves every stage.")
-    terraformx_destroy.add_argument("-refresh-only", action="store_true", help = "Review how terraform would update your state file.")
-    terraformx_destroy.add_argument("-destroy-history", action="store_true", help = "Destroys all in terraform_history.csv.")
+    terraformx_destroy.add_argument(Args.DIR, type = str, default=Default.EMPTY_STRING, help = Help.LOCATION_OF_TERRAFORM_ROOT)
+    # terraformx_apply.add_argument(Args.VAR_FILE, type = str, default=Default.EMPTY_STRING, help = Help.LOCATION_OF_TFVARS_FILE)
+    terraformx_destroy.add_argument(Args.AUTO_APPROVE, action=Action.STORE_TRUE, help = Help.AUTO_APPROVE_COMMAND)
+    terraformx_destroy.add_argument(Args.OVERRIDE_WORKFLOW, action=Action.STORE_TRUE, help = Help.OVERRIDE_WORKFLOW)
+    terraformx_destroy.add_argument(Args.REFRESH_ONLY, action=Action.STORE_TRUE, help = Help.TERRAOFORM_STATE_FILE_REVIEW)
+    terraformx_destroy.add_argument(Args.DESTROY_HISTORY, action=Action.STORE_TRUE, help = Help.DESTORY_ALL_IN_HISTORY)
 
-    terraformx_output = subparsers.add_parser("output")
+    terraformx_output = subparsers.add_parser(Parser.OUTPUT)
     terraformx_output.set_defaults(function=output)
-    terraformx_output.add_argument("-dir", type = str, default="", help = "Location of terraform root.")
+    terraformx_output.add_argument(Args.DIR, type = str, default=Default.EMPTY_STRING, help = Help.LOCATION_OF_TERRAFORM_ROOT)
     
     # all_parsers = [
     #     terraformx_init, 
@@ -64,11 +94,11 @@ def main():
 
     # for parser in all_parsers:
     #     # arguments
-    #     parser.add_argument("-dir", type = str, default="", help = "Location of terraform root.")
-    #     # parser.add_argument("-var-file", type = str, default="", help = "Location of variable definitions file.")
-    #     parser.add_argument("-auto-approve", action="store_true", help = "Auto approve command.")
-    #     parser.add_argument("-override-workflow", action="store_true", help = "Overrides workflow stages auto_approve keys and auto approves every stage.")
-    #     parser.add_argument("-refresh-only", action="store_true", help = "Review how terraform would update your state file.")
+    #     parser.add_argument(Args.DIR, type = str, default=Default.EMPTY_STRING, help = "Location of terraform root.")
+    #     # parser.add_argument(Args.VAR_FILE, type = str, default=Default.EMPTY_STRING, help = Help.LOCATION_OF_TFVARS_FILE)
+    #     parser.add_argument(Args.AUTO_APPROVE, action=Action.STORE_TRUE, help = Help.AUTO_APPROVE_COMMAND)
+    #     parser.add_argument(Args.OVERRIDE_WORKFLOW, action=Action.STORE_TRUE, help = Help.OVERRIDE_WORKFLOW)
+    #     parser.add_argument(Args.REFRESH_ONLY, action=Action.STORE_TRUE, help = Help.TERRAOFORM_STATE_FILE_REVIEW)
     
     # parse the arguments and call the right function
     args = top_level_parser.parse_args()
