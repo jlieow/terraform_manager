3e8ff91af7f7df7324818e3a5cc9f31150807bce
jlieow <52845474+jlieow@users.noreply.github.com>
2023-02-04 15:45:21 +0800

    Completed new feature, github action: terraformx_apply

--- terraformx/__init__.py
+++ terraformx/__init__.py
@@ -1,12 +1,7 @@
-# from .terraformx import *
-from .terraformx_common import *
-from .parser_init import *
-from .parser_apply import *
-from .parser_destroy import *
-from .parser_output import *
-from .parser_blueprints import *
-
-
-
-
-
+# # from .terraformx import *
+# from .terraformx_common import *
+# from .parser_init import *
+# from .parser_apply import *
+# from .parser_destroy import *
+# from .parser_output import *
+# from .parser_blueprints import *
