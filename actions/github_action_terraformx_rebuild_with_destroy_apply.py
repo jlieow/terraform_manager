6977c05aebdfbc207929a3cc4da0f4290a69a37d
jlieow <52845474+jlieow@users.noreply.github.com>
2023-02-07 23:09:19 +0800

    Refactoring actions rebuild to use github_action_apply_only and github_action_destroy_only for rebuild

--- actions/github_action_terraformx_rebuild_with_destroy_apply.py
+++ actions/github_action_terraformx_rebuild_with_destroy_apply.py
@@ -3,8 +3,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from terraformx.parser_destroy import *
-from terraformx.parser_apply import *
+from actions.github_action_terraformx_apply import *
+from actions.github_action_terraformx_destroy import *
 
 def main():
     cwd = os.getcwd()
@@ -15,8 +15,11 @@ def main():
     
     tfvars_settings(cwd) 
     terraform_init(cwd)
-    destroy_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action=True)
-    apply_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action=True)
+    # destroy_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action=True)
+    # apply_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action=True)
+
+    github_action_destroy_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action_active_stages="")
+    github_action_apply_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action_active_stages="")
 
 if __name__ == "__main__":
     main()
