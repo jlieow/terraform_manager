99fc2fe1dab12c76575c0ad30d1d2ca645b929e5
jlieow <52845474+jlieow@users.noreply.github.com>
2023-02-07 23:51:17 +0800

    Incorporated -active-stages for github_action_terraformx_apply, ...destroy and ...rebuild

--- actions/github_action_terraformx_rebuild_with_destroy_apply.py
+++ actions/github_action_terraformx_rebuild_with_destroy_apply.py
@@ -7,6 +7,15 @@ from actions.github_action_terraformx_apply import *
 from actions.github_action_terraformx_destroy import *
 
 def main():
+
+    # top-level parser
+    top_level_parser = argparse.ArgumentParser(description = "terraformx")
+    top_level_parser.add_argument(Args_constants.ACTIVE_STAGES, type = str, default=Default_constants.EMPTY_STRING, help = Help_constants.ACTIVE_STAGES)
+
+    args = top_level_parser.parse_args()
+    
+    active_stages = args.active_stages
+
     cwd = os.getcwd()
 
     if not os.path.exists(cwd + "/backend.tf"):
@@ -18,8 +27,8 @@ def main():
     # destroy_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action=True)
     # apply_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action=True)
 
-    github_action_destroy_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action_active_stages="")
-    github_action_apply_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action_active_stages="")
+    github_action_destroy_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action_active_stages=active_stages)
+    github_action_apply_only(cwd, var_file="", auto_approve=False, override_workflow=False, github_action_active_stages=active_stages)
 
 if __name__ == "__main__":
     main()
